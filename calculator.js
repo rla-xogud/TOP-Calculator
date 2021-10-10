d1b5497ca4f2d8eb48d96d0dfc4c93decae7629b
rla-xogud <tae.h.kim@outlook.com>
2021-10-10 02:13:47 -0500

    Cleaned up some abstraction code, added comments

--- calculator.js
+++ calculator.js
@@ -1,8 +1,8 @@
-//let inputs = document.getElementById("calculatorInput");
-const inputs = new Array(3);
-let equalsFlag = false;
-let operationFlag = false;
+const inputs = new Array(3); // [firstNumber, operator, secondNumber]
+let equalsFlag = false; //flag to see if equals was the last operation performed
+let operationFlag = false;//flag to see if operation button was clicked. Needed for operation changes
 
+//Event delegation for input buttons and clearing
 document.getElementById("calculatorInput").addEventListener("click", function(event){
     if(inputs[1] != null){
         document.getElementById("calculatorScreen").innerHTML = 0;
@@ -17,58 +17,31 @@ document.getElementById("calculatorInput").addEventListener("click", function(ev
                 numberLogic("1Btn");
                 break;
             case "2Btn":
-                document.getElementById("calculatorScreen").innerHTML = Number(document.getElementById("calculatorScreen").innerHTML +"2")
-                if(operationFlag == true){
-                    inputs[2] = document.getElementById("calculatorScreen").innerHTML;
-                }
+                numberLogic("2Btn");
                 break;     
             case "3Btn":
-                document.getElementById("calculatorScreen").innerHTML = Number(document.getElementById("calculatorScreen").innerHTML +"3")
-                if(operationFlag == true){
-                    inputs[2] = document.getElementById("calculatorScreen").innerHTML;
-                }
+                numberLogic("3Btn");
                 break;
             case "4Btn":
-                document.getElementById("calculatorScreen").innerHTML = Number(document.getElementById("calculatorScreen").innerHTML +"4")
-                if(operationFlag == true){
-                    inputs[2] = document.getElementById("calculatorScreen").innerHTML;
-                }
+                numberLogic("4Btn");
                 break;
             case "5Btn":
-                document.getElementById("calculatorScreen").innerHTML = Number(document.getElementById("calculatorScreen").innerHTML +"5")
-                if(operationFlag == true){
-                    inputs[2] = document.getElementById("calculatorScreen").innerHTML;
-                }
+                numberLogic("5Btn");
                 break;
             case "6Btn":
-                document.getElementById("calculatorScreen").innerHTML = Number(document.getElementById("calculatorScreen").innerHTML +"6")
-                if(operationFlag == true){
-                    inputs[2] = document.getElementById("calculatorScreen").innerHTML;
-                }
+                numberLogic("6Btn");
                 break;     
             case "7Btn":
-                document.getElementById("calculatorScreen").innerHTML = Number(document.getElementById("calculatorScreen").innerHTML +"7")
-                if(operationFlag == true){
-                    inputs[2] = document.getElementById("calculatorScreen").innerHTML;
-                }
+                numberLogic("7Btn");
                 break;
             case "8Btn":
-                document.getElementById("calculatorScreen").innerHTML = Number(document.getElementById("calculatorScreen").innerHTML +"8")
-                if(operationFlag == true){
-                    inputs[2] = document.getElementById("calculatorScreen").innerHTML;
-                }
+                numberLogic("8Btn");
                 break;
             case "9Btn":
-                document.getElementById("calculatorScreen").innerHTML = Number(document.getElementById("calculatorScreen").innerHTML +"9")
-                if(operationFlag == true){
-                    inputs[2] = document.getElementById("calculatorScreen").innerHTML;
-                }
+                numberLogic("9Btn");
                 break;
             case "0Btn":
-                document.getElementById("calculatorScreen").innerHTML = Number(document.getElementById("calculatorScreen").innerHTML +"0")
-                if(operationFlag == true){
-                    inputs[2] = document.getElementById("calculatorScreen").innerHTML;
-                }
+                numberLogic("0Btn");
                 break;
             case "clearBtn":
                 clear();
@@ -79,52 +52,44 @@ document.getElementById("calculatorInput").addEventListener("click", function(ev
     }
 });
 
-    document.getElementById("calculatorOperations").addEventListener("click", function(event){
-        //if(equalsFlag == true) equalsFlag = false;
-        operationFlag = true;
-        if(event.target.className == "calculatorButton"){
-            if(inputs[0] == null){
-                inputs[0] = document.getElementById("calculatorScreen").innerHTML;
-            }
-            switch(event.target.id){
-                case "addBtn":
-                    //operation function
-                    inputs[1] = "+"
-//                    inputs[2] = document.getElementById("calculatorScreen").innerHTML
-                    break;
-                case "subBtn":
-                    //operation function
-                    inputs[1] = "-"
-                    break;     
-                case "multBtn":
-                    //operation function of inputs
-                    inputs[1] = "*"
-                    break;
-                case "divBtn":
-                    //operation function of inputs
-                    inputs[1] = "/"
-                    break;    
-                case "equalsBtn":
-                    //operation function of inputs
-                    operation();
-                    equalsFlag = true;
-                    operationFlag = false;
-                    break;
-            }
-            
+//Event delegation for operations
+document.getElementById("calculatorOperations").addEventListener("click", function(event){
+    //if(equalsFlag == true) equalsFlag = false;
+    operationFlag = true;
+    if(event.target.className == "calculatorButton"){
+        if(inputs[0] == null){
+            inputs[0] = document.getElementById("calculatorScreen").innerHTML;
         }
-    });
+        switch(event.target.id){
+            case "addBtn":
+                //operation function
+                inputs[1] = "+"
 
-    
-    // if(event.target.className == "calculatorButton" && event.target.id =="1Btn"){
-    //     alert("hello")
-    //     document.getElementById("calculatorScreen").innerHTML = Number(document.getElementById("calculatorScreen").innerHTML +"1")
-    // }
-    //else if(event.target.className =="indivDiv"){
-    //     event.target.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-    // }
+                break;
+            case "subBtn":
+                //operation function
+                inputs[1] = "-"
+                break;     
+            case "multBtn":
+                //operation function of inputs
+                inputs[1] = "*"
+                break;
+            case "divBtn":
+                //operation function of inputs
+                inputs[1] = "/"
+                break;    
+            case "equalsBtn":
+                //operation function of inputs
+                operation();
+                equalsFlag = true;
+                operationFlag = false;
+                break;
+        }
+        
+    }
+});
 
-function operation(){
+ function operation(){
     let firstNum = Number(inputs[0]);
     if(inputs[2] == null){
         inputs[2] = document.getElementById("calculatorScreen").innerHTML
@@ -151,6 +116,7 @@ function operation(){
         case "/":
             if(secondNum === 0){
                 alert("Don't divide by zero. What are you doing?");
+                clear();
                 return;
             }
             answer = firstNum / secondNum;
@@ -173,7 +139,7 @@ function clear(){
 function numberLogic(str){
     let number = str[0]
     document.getElementById("calculatorScreen").innerHTML = Number(document.getElementById("calculatorScreen").innerHTML + number)
-                if(operationFlag == true){
-                    inputs[2] = document.getElementById("calculatorScreen").innerHTML;
-                }
+    if(operationFlag == true){
+        inputs[2] = document.getElementById("calculatorScreen").innerHTML;
+    }
 }
